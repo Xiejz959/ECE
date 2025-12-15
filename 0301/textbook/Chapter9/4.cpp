8ec43d8eda58375ca935a9d22d045d08b5afbaf9
Xie Junze <Xiejz051030@outlook.com>
2025-12-15 08:13:02 +0800

    chap 9

---
+++ 0301/textbook/Chapter9/4.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+struct stu
+{
+    int score;
+    string name;
+};
+void sort(stu*, int);  
+double average(stu*, int); 
+int main()
+{
+    stu *ScorePtr = nullptr;
+	int Scores = 0;			
+	double Average = 0;			
+	cout << "How many test scores will you enter? "<< endl;
+	cin >> Scores;
+	while (Scores < 0)
+	{
+		cout << "The number cannot be negative."<< endl;
+		cin >> Scores;
+	}
+	ScorePtr = new stu[Scores];
+	for (int i = 0; i < Scores; i++)
+	{
+		cout << "Enter test score "<< (i + 1) << ": ";
+		cin >> (ScorePtr+i)->score;
+        cin >> (ScorePtr+i)->name;
+		while ((ScorePtr+i)->score < 0)  
+		{
+			cout << "Negative scores are not allowed."<< endl;
+			cin >> (ScorePtr+i)->score;
+		}
+	}
+	sort(ScorePtr, Scores);
+	Average = average(ScorePtr, Scores);
+	cout << "\nThe test scores in ascending "<< "order, and their average, are:"<< endl;
+	for (int j = 0; j < Scores; j++)
+	{
+		cout << fixed << setprecision(2) << setw(6) << (ScorePtr+j)->score << " " << (ScorePtr+j)->name << endl;
+	}
+	cout << "Average score:  " << setprecision(2) << fixed << Average << endl << endl;
+	delete [] ScorePtr;
+	ScorePtr = nullptr;
+	return 0;
+}
+void sort(stu* score, int size)
+{
+	int start, minIndex; 
+	double minValue;
+    // sorting in ascending order
+	for (start = 0; start < (size - 1); start++)
+	{
+		minIndex = start;
+		minValue = (score+start)->score;
+
+		for(int index = start + 1; index < size; index++)
+		{
+			if ((score+index)->score < minValue)
+			{
+				minValue = (score+index)->score;
+				minIndex = index;
+			}
+		}
+
+		*(score+minIndex) = *(score+start);
+		(score+start)->score = minValue;
+	}
+}
+double average(stu* score, int numScores)
+{
+	double total = 0;
+	for (int k = 0; k < numScores; k++)
+		total += (score+k)->score;
+   
+	// Return the average score.
+	return (total / numScores);
+}
